c44f37dc62ffa58ccc568bf6992dc49d9cde7f6f
tom-mats <dawn.gray.sky@gmail.com>
2017-08-19 12:46:02 +0900

    add parameters for db

--- db/migrate/001_create_issue_reviewers.rb
+++ db/migrate/001_create_issue_reviewers.rb
@@ -1,10 +1,10 @@
 class CreateIssueReviewers < ActiveRecord::Migration
   def change
     create_table :issue_reviewers do |t|
-      t.boolean :is_done
+      t.boolean :is_done, :default => false
       t.string :reviewer
-      t.interger :position
-      t.references :issue, index: true, foreign_key: true
+      t.interger :position :default => 1
+      t.references :issue, index: true, foreign_key: true, null: false
     end
     add_index :issue_reviewers, :issue_id
   end
