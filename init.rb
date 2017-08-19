28108eae888a2e3c4117aa896d8185299f4eaa10
tom-mats <dawn.gray.sky@gmail.com>
2017-08-19 13:59:56 +0900

    Add plugin settings

--- init.rb
+++ init.rb
@@ -5,4 +5,7 @@ Redmine::Plugin.register :redmine_multi_reviewer do
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
+  settings default: {
+    reviewers_log: true
+  }, partial: 'settings/issue_reviewers'
 end
